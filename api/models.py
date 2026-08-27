d93ce0fdc7ed146115fb517eac6a90ca00ebb517
nicolass03 <39010852+nicolass03@users.noreply.github.com>
2026-08-27 23:34:03 +0200

    fix: api error

--- api/models.py
+++ api/models.py
@@ -80,12 +80,6 @@ class SessionSegment(Base):
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now()
     )
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        nullable=False,
-        server_default=func.now(),
-        onupdate=func.now(),
-    )
 
     session: Mapped["WorkoutSession"] = relationship(back_populates="segments")
 
